2243be601fff43aaea868b254df9d5f4e85b9abb
Kate465 <kategokeborate@gmail.com>
2024-10-09 19:00:51 +0100

    added js to project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
+function updateTime() {
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = document.querySelector(".date");
+  let losAngelesTimeElement = document.querySelector(".time");
+  let losAngelesTime = moment().tz("America/Los_Angeles");
 
+  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+  losAngelesTimeElement.innerHTML = losAngelesTime.format(
+    "h:mm:ss [<small>] A [</small>]"
+  );
+
+  let parisElement = document.querySelector("#paris");
+  let parisDateElement = parisElement.querySelector(".date");
+  let parisTimeElement = parisElement.querySelector(".time");
+  let parisTime = moment().tz("Europe/Paris");
+  console.log(parisTime);
+
+  parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
+  parisTimeElement.innerHTML = parisTime.format(
+    "h:mm:ss [<small>] A [</small>]"
+  );
+}
+updateTime();
+setInterval(updateTime, 1000);
